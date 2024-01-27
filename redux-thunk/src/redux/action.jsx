ebdf6ac32811491f4c338ef876e96e203337402d
Dhruvil Sheth <dhruvilsheth@proton.me>
2024-01-27 15:51:28 +0530

    done

---
+++ redux-thunk/src/redux/action.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const dataFetching = () => (dispatch) => {
+    axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+        console.log(res)
+        dispatch(fetchingDataFunction(res.data));
+    })
+    .catch((err) => console.log("Not Found",err))
+}
+
+export const fetchingDataFunction =(payload) => {
+    return{type:"fetchData",payload};
+}
